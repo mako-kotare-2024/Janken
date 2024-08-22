928336a67d42475fc03bc310c229bb5618305279
TheFlyingDeutchMan <tim.gerschner.nz@gmail.com>
2024-08-23 11:56:37 +1200

    general improvement to previous

--- client/components/Game.tsx
+++ client/components/Game.tsx
@@ -9,6 +9,9 @@ function Game() {
   return (
     <div className="Game">
       <h2>Game</h2>
+      {/* STATE THINGS GO UNDER THE GAME */}
+
+      
       <div className="buttonGroup">
         <button className="rock">
           <img src="/images/choose-rock.png" alt="Choose Rock" />
@@ -20,14 +23,20 @@ function Game() {
           <img src="/images/choose-paper.png" alt="Choose Paper" />
         </button>
       </div>
+      
+      {/* PLAYER THINGS */}
       <div className="player">
-        <p>player</p>
+        <img src="/images/alex.png" alt="The player prepping"/>
         {/* Player Image here */}
       </div>
+      
+      {/* COMPUTER THINGS */}
       <div className="computer">
         <p>computer</p>
         {/* Enemy Image here */}
       </div>
+      
+      {/* just bricks, no biggie */}
       <div className="bricks">
         <p>bricks</p>
         {/* Bricks for the people can't fly */}
---
+++ client/components/Player.tsx
@@ -0,0 +1,18 @@
+// Sends request to the database for the correct player images.
+import PlayerSelect from "../../game"
+
+// TODO: THIS IS TIM'S MINUTE UNDERSTANDING ON THE STATE SYSTEM. OVERHAUL AND IMPROVE PLEASE!!!
+export default function PlayerState(states) {
+    <img src="/images/alex.png" alt="Player readying move" />
+    if (PlayerSelect === 1) {
+        return (
+            <img src="/images/alex-rock.png" alt="Player selected rock" />
+        )
+    } else if (PlayerSelect === 2) {
+        return (
+            <img src="/images/alex-paper.png" alt="Player selected paper" />
+        )
+    } else if (PlayerSelect === 3) {
+        <img src="/images/alex-scissors.png" alt="Player selected Scissors"
+    }
+}
